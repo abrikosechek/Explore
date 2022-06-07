7e88033af1a76770cc57bc85a6492b5a826c061e
abrikosechek <abrikosechek@gmail.com>
2022-06-07 14:48:08 +0400

    error(

---
+++ client/src/components/MainPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main_page">
+    <img src="../assets/mp_bg.png" class="mp_bg" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainPage",
+};
+</script>
+
+<style lang="scss">
+.main_page {
+  width: 100%;
+
+  .mp_bg {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
---
+++ client/src/components/NavigBar.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="navbar container">
+    <router-link to="/main" class="nav_logo">Explore</router-link>
+    <div class="nav_links">
+      <router-link to="/main" class="nav_link">home</router-link>
+      <router-link to="/main" class="nav_link">tours</router-link>
+      <router-link to="/main" class="nav_link">hotel</router-link>
+      <router-link to="/main" class="nav_link">gallery</router-link>
+      <router-link to="/main" class="nav_link">pages</router-link>
+      <router-link to="/main" class="nav_link">blog</router-link>
+      <router-link to="/main" class="nav_link">contact</router-link>
+      <router-link to="/main" class="nav_link">login</router-link>
+      <router-link to="/main" class="nav_link">Sign UP</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavigBar",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+.navbar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 20px;
+
+  .nav_logo {
+    font-weight: 600;
+    font-size: 36px;
+    color: white;
+  }
+
+  .nav_links {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+
+    .nav_link {
+      display: block;
+      height: 100%;
+      padding: auto 10px;
+      font-weight: 500;
+      font-size: 16px;
+
+      &:last-child {
+        display: block;
+        height: 100%;
+        padding: auto 20px;
+        background-color: #ffc814;
+        font-weight: 500;
+        font-size: 16px;
+        color: #1f3347;
+      }
+    }
+  }
+}
+</style>
